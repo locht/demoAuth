12020b24c3fff0129d893a4b5cce7840ead27796
locht <thanhloclhu@gmail.com>
2020-08-30 23:15:18 +0700

    1

--- demoAuth/account/api/urls.py
+++ demoAuth/account/api/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
-from account.api.views import RegistrationView
-
-from rest_framework.authtoken import views
-
+from account.api.views import RegistrationView #, CustomAuthToken
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 app_name = 'account'
 
 urlpatterns = [
-    path('register', RegistrationView, name='register'),
+    path('register', RegistrationView.as_view(), name='register'),
     path('login', obtain_auth_token, name='login'),
-    path('view', views.obtain_auth_token, name='view_api'),
+    # path('view', CustomAuthToken.as_view(), name='view_api')
 ]
